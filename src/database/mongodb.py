0fdf1358ad00c1a8f53928268d74c7a89a29832b
yashwanthreddyGoduguchintha <reddyyashu20@gmail>
2022-10-16 14:39:33 +0530

    second commit

--- src/database/mongodb.py
+++ src/database/mongodb.py
@@ -11,7 +11,7 @@ class MongodbOperation:
 
         #self.client = pymongo.MongoClient(os.getenv('MONGO_DB_URL'),tlsCAFile=ca)
         #self.db_name="ineuron"
-        MONGO_DB_URL= "mongodb+srv://yash2:yash @cluster0.s9tlh9d.mongodb.net/?retryWrites=true&w=majority"
+        MONGO_DB_URL= "mongodb+srv://yash2:yash12345 @cluster0.s9tlh9d.mongodb.net/?retryWrites=true&w=majority"
         self.client = pymongo.MongoClient(MONGO_DB_URL,tlsCAFile=ca)
         self.db_name="sensor_DB"
 
